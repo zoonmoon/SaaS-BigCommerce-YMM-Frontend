b4ee79f3414ffcd1eb6914df36fde536477d472f
Arjun Poudel <arjunpoudel479@gmail.com>
2025-04-24 05:54:56 +0545

    progress-save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+'use client'
 
-function App() {
+import { useEffect, useState } from "react";
+
+import { SpecsDropdownWidget } from "./components/specs-dropdown-widget";
+import ProductGridAndFilters from "./components/product-grid-and-filters";
+import { Route, Link, Switch } from "wouter";
+import { useRouter } from 'wouter';
+
+
+function ProductsBySpecs(){
+  return(
+    <ProductGridAndFilters />
+  )
+}
+
+function SpecsDropdownWidgetInline(){
+
+  const { push } = useRouter();  // Get the push method for navigation
+
+  const callBack = (selectedSpecs) => {
+    window.location.href = '/compatible-parts?fits='+selectedSpecs;  //
+  };
+
+  console.log("ENV URL from componenet:", process.env.REACT_APP_API_URL);
+
+  return(
+    <SpecsDropdownWidget 
+      endpoint={`${process.env.REACT_APP_API_URL}/api/storefront/dropdown-specs`}
+      callbackToSubmission={callBack}
+      storeHash={process.env.REACT_APP_STORE_HASH}
+    /> 
+  )
+
+}
+
+const App = () => {
+
+  console.log("ENV URL:", process.env.REACT_APP_API_URL);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav style={{marginBottom:'20px'}}>
+        <Link href="/">Home</Link> | <Link href="/compatible-parts/">Products by Parts</Link>
+      </nav>
+      
+      <Switch>
+        <Route path="/" component={SpecsDropdownWidgetInline} />
+        <Route path="/compatible-parts" component={ProductsBySpecs} />
+        <Route>404 - Not Found</Route>
+      </Switch>
     </div>
   );
-}
+};
+
+// function App() {
+  
+//   return (
+
+//     <div className="App">
+      
+//       <SpecsDropdownWidget 
+//         endpoint={'http://localhost:3000/api/storefront/dropdown-specs'}
+//         callbackToSubmission={() => alert(1)}
+//         storeHash={'bohaxauo'}
+//       />  
+
+//     </div>
+//   );
+// }
 
 export default App;
